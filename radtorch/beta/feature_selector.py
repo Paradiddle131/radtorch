411a7d869c3926dfb0cb97a14a15a64303fdf76d
elbanan <elbanan@users.noreply.github.com>
2020-08-23 21:19:41 -0400

    fpr feature selector

---
+++ radtorch/beta/feature_selector.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 RADTorch and Mohamed Elbanan, MD
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/
+
+# Documentation update: 5/11/2020
+
+from ..settings import *
+from ..utils import *
+
+from .dataset import *
+from .data_processor import *
+
+
+class Feature_Selector():
